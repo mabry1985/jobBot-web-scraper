049595414c701ff6bf0011351e25708e264761b0
Josh Mabry <mabry1985@gmail.com>
2019-12-05 15:48:46 -0800

    refactor main function

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const cheerio = require("cheerio");
 
-let searchQuery = [
-  "web development"
+let jobSample = [
+  {
+    jobTitle: "Junior React Developer",
+    jobDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad animi unde nisi consequatur dolore quaerat distinctio similique at! Incidunt, omnis! Porro sint delectus vel minus laboriosam asperiores totam vitae ullam minima! Enim, dolore! Deleniti iure beatae, modi, mollitia aperiam, perspiciatis est autem ipsum sunt voluptas architecto corrupti aliquam. Quasi, quidem!",
+    company: "Planet Argon",
+    applicationUrl: "https://www.planetargon.com",
+  }
 ]
 
+
+let searchQuery = [
+  "web development",
+  "junior developer",
+  "react developer",
+  "javascript developer",
+]
+
+let search
 
 let siteList = [
   {
     company: "Google Jobs",
     url: `https://www.google.com/search?rlz=1C5CHFA_enUS860US860&sxsrf=ACYBGNQoashCFDMDYOW6Aije_c9gIgCenQ:1575587263331&ei=v43pXZ-8E6vO0PEP2f-myA8&q=
-    ${searchQuery.replace(" ", "+")}+teset&ibp=htl;jobs&sa=X&ved=2ahUKEwj2mIqA0J_mAhVNHjQIHZKTDqkQiYsCKAB6BAgKEAM`
+    ${search}&ibp=htl;jobs&sa=X&ved=2ahUKEwj2mIqA0J_mAhVNHjQIHZKTDqkQiYsCKAB6BAgKEAM`
   },
   {
     company: "Dice",
@@ -21,21 +35,21 @@ let siteList = [
 
 async function main() {
   const browser = await puppeteer.launch({ headless: false });
-  const $ = cheerio
   for (let i = 0; i < siteList.length; i++){
     const page = await browser.newPage();
-    await page.goto(siteList[i].url);
-    const html = await page.content();
-
-    switchFunction(siteList[i].company, searchQuery)
+    const site = siteList[i];
+    await page.goto(site.url);
+    switchFunction(site.company, site.url)
   }
 }
 
-function switchFunction(company, searchQuery) {
+function switchFunction(company) {
   switch(company){ 
     case "Google Jobs":
-     for(i = 0; i < searchQuery[i]; i++) {
-       scrapeGoogleJobs(searchQuery[i])
+     for(i = 0; i < searchQuery.length; i++) {
+       search = searchQuery[i].replace(" ", "+");
+      //  scrapeGoogleJobs(searchQuery[i])
+      console.log(search);
      }
     // case "Dice":
     //  scrapeDice(searchQuery)
@@ -44,7 +58,7 @@ function switchFunction(company, searchQuery) {
 } 
 
 async function scrapeGoogleJobs(searchQuery) {
-
+  
 }
 
 // async function scrapeDice
@@ -58,7 +72,7 @@ function writeFiles(companyName, html) {
   
   try {
     if (fs.existsSync(path)) {
-      ggconst newPath = `./${companyName}-new.txt`
+      const newPath = `./${companyName}-new.txt`
       fs.writeFileSync(newPath, html);
       compareFiles(path, newPath);
     } else 
