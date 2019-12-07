19a5c2543583ded62486c9022c866e7821003214
Josh Mabry <mabry1985@gmail.com>
2019-12-06 18:05:56 -0800

    refactor code

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function switchFunction(company) {
   }    
 } 
   
-async function scrapeGoogleJobTitles(html) {
+async function createGoogleJobObjects(html) {
   const $ = cheerio.load(html);
   const titles = $("#gws-horizon-textlists__job_details_page").map((i, el) => { 
     const title = $(el).find("h2").text();
@@ -55,12 +55,13 @@ async function googleScrape(browser) {
   searchQuery.map( async (el, i) => {
     let search = el.replace(" ", "+");
     let url = `https://www.google.com/search?q=${search}&rlz=1C5CHFA_enUS860US860&oq=softwar&aqs=chrome.0.69i59j0j69i59j69i57j69i60j69i65.2127j1j4&sourceid=chrome&ie=UTF-8&ibp=htl;jobs&sa=X&ved=2ahUKEwjh4pSa5Z_mAhXPIjQIHb6yDMEQiYsCKAB6BAgCEAM#htivrt=jobs&fpstate=tldetail&htichips=date_posted:today&htischips=date_posted;today&htidocid=D8bry7zLg-f08MJsAAAAAA%3D%3D`
+
     try {
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: "networkidle2" })
       const html = await page.evaluate(() => document.body.innerHTML, console.log(search, "done"));
 
-      let jobs = await scrapeGoogleJobTitles(html)
+      let jobs = await createGoogleJobObjects(html)
       console.log(jobs)
     } catch(err) {
       console.error(err)
