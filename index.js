695ac03d850d52a79bc18fd075ab8676640d995d
Josh Mabry <mabry1985@gmail.com>
2019-12-07 13:59:39 -0800

    refactor code

--- index.js
+++ index.js
@@ -34,26 +34,26 @@ async function connectToMongoDb() {
     console.log("Connected to DB")
   }
   
-
 async function addSearchQuery(query) {
   const searchQuery = new SearchQuery({
     query
   })
   await searchQuery.save(function(err) {
     if (err) return handleError(err);
-      console.log("Saved new search query");
+    console.log("Saved new search query");
   });
-}
-
-async function searchModel() {
-    
 }
 
 async function main() {
   await connectToMongoDb();
-  queries = await SearchQuery.find()
-  for (let i = 0; i < siteList.length; i++){
-    switchFunction(siteList[i].company, queries)
+  // await addSearchQuery("web developer")
+  queries = await SearchQuery.find();
+  await siteLoop(queries);
+}
+
+async function siteLoop(queries) {
+  for (let i = 0; i < siteList.length; i++) {
+    switchFunction(siteList[i].company, queries);
   }
 }
 
