9addd748407bc8d09e11a6415c6c2125dbb00bd1
Josh Mabry <mabry1985@gmail.com>
2019-12-07 18:01:54 -0800

    add file utility, add logic to save to csv file

--- utility/files.js
+++ utility/files.js
@@ -1,18 +1,10 @@
-function writeFiles(companyName, html) {
-  const path = `./${companyName}.txt`
+const ObjectsToCsv = require("objects-to-csv");
 
-  try {
-    if (fs.existsSync(path)) {
-      const newPath = `./${companyName}-new.txt`
-      fs.writeFileSync(newPath, html);
-      compareFiles(path, newPath);
-    } else
-      fs.writeFileSync(path, html);
-  } catch (err) {
-    console.error(err)
-  }
+async function createCsvFile(data, company) {
+  const csv = new ObjectsToCsv(data);
+  await csv.toDisk(`./${company}.csv`);
 }
 
-function compareFiles(file1, file2) {
-  console.log(file1 === file1)
+module.exports = {
+  createCsvFile
 }
