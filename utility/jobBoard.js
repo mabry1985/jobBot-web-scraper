2a6c40816f616793039a1a33abade1fb6f15b574
Josh Mabry <mabry1985@gmail.com>
2019-12-08 15:33:51 -0800

    add job board site to save

--- utility/jobBoard.js
+++ utility/jobBoard.js
@@ -1,20 +1,20 @@
 const JobBoard = require("../models/JobBoard");
 
-async function save(title, description, postedBy, applyUrl) {
+async function save(title, description, postedBy, applyUrl, jobBoardSite) {
   const jobFromDb = await JobBoard.findOne({ description });
   if (!jobFromDb && !title.includes("Senior") && !title.includes("Sr") && !title.includes("SR") ) {
     const jobBoard = new JobBoard({
       title,
       description,
       postedBy,
-      applyUrl
+      applyUrl,
+      jobBoardSite
     });
     console.log("job saved");
     return jobBoard.save();
   } else {
     console.log("duplicate job found");
   }
-  return jobs
 }
 
 module.exports = {
