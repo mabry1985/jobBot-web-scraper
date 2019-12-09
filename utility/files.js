7f5ae991829443e93173febe639c96ae37747ac9
Josh Mabry <mabry1985@gmail.com>
2019-12-09 09:55:05 -0800

    add option to append to existing csv

--- utility/files.js
+++ utility/files.js
@@ -1,8 +1,9 @@
 const ObjectsToCsv = require("objects-to-csv");
 
 async function createCsvFile(data) {
-  const csv = new ObjectsToCsv(data);
-  await csv.toDisk(`./jobs.csv`);
+  const csv = new ObjectsToCsv(data)
+  await csv.toDisk('./jobs.csv', { append: true });
+  console.log("csv written");
 }
 
 module.exports = {
