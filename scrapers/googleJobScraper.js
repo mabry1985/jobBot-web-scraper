e893a88e178bc01f21f6bf6005ab6e0271bff4c1
Josh Mabry <mabry1985@gmail.com>
2019-12-09 09:51:01 -0800

    fix promise undefinded returns and add create csv function for jobs

--- scrapers/googleJobScraper.js
+++ scrapers/googleJobScraper.js
@@ -1,31 +1,37 @@
 const cheerio = require('cheerio');
 const jobBoard = require('../utility/jobBoard');
-const downloadTestHtmlFile = false;
+const file = require('../utility/files');
 
 async function createGoogleJobObjects(html) {
   const $ = cheerio.load(html);
-  const results = $("#gws-horizon-textlists__job_details_page")
-    .map(async (i, el) => {
-      const title = $(el)
-        .find("h2")
-        .text();
-      const description = $(el)
-        .find("span[style='line-height:1.5em']")
-        .text();
-      const postedBy = $(el)
-        .find(" > div > div.kFb3Ed > span")
-        .text();
-      const applyUrl = $(el)
-        .find(" span > a")
-        .attr("href")
-        .toString();
-      await jobBoard.save(title, description, postedBy, applyUrl, "Google Jobs")
-    }).get();
-  return results;
+  const results = Promise.all(
+    $("#gws-horizon-textlists__job_details_page")
+      .map(async (i, el) => {
+        const title = $(el)
+          .find("h2")
+          .text();
+        if(jobBoard.jobFilterTitle(title)){
+          return;
+        }
+        const description = $(el)
+          .find("span[style='line-height:1.5em']")
+          .text();
+        const postedBy = $(el)
+          .find(" > div > div.kFb3Ed > span")
+          .text();
+        const applyUrl = $(el)
+          .find(" span > a")
+          .attr("href")
+          .toString();
+        const timeStamp = new Date();
+        jobBoard.save(title, description, postedBy, applyUrl, timeStamp, "Google Jobs")
+        return{ title, description, postedBy, applyUrl }
+      }).get()
+    )
+    return Promise.resolve(results)
 }
 
 async function googleScrape(browser, queries) {
-  console.log(queries);
   queries.map(async el => {
     let search = el.query.replace(" ", "+");
     let url = `https://www.google.com/search?rlz=1C5CHFA_enUS860US860&sxsrf=ACYBGNQFBvGV7oqKx8JbHS7Bl7RZYx-n_A:1575752693783&ei=9RPsXamkL8ba-gTL1IXYDw&q=${search}&gs_l=psy-ab.3..0l2j0i131j0l7.70925.72801..73402...2.2..1.103.563.6j1......0....1..gws-wiz.......0i71j0i67j0i10i67j0i10j0i13.qs7MRgeogIE&uact=5&ibp=htl;jobs&sa=X&ved=2ahUKEwjk3pfAuKTmAhXCtp4KHbSuB5AQiYsCKAB6BAgKEAM#htivrt=jobs&fpstate=tldetail&htichips=date_posted:today&htischips=date_posted;today&htilrad=48.2802&htidocid=sFqvZk2-c-3VV-g5AAAAAA%3D%3D`;
@@ -34,14 +40,8 @@ async function googleScrape(browser, queries) {
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: "networkidle2" });
       const html = await page.evaluate(() => document.body.innerHTML);
-
-      // global switch variable
-      if (downloadTestHtmlFile) {
-        fs.writeFileSync("./googleJobBoard.html", html);
-      }
-
-      await createGoogleJobObjects(html);
-
+      const jobs = await createGoogleJobObjects(html)
+      file.createCsvFile(jobs)
     } catch (err) {
       console.error(err);
     }
@@ -49,5 +49,6 @@ async function googleScrape(browser, queries) {
 }
 
 module.exports = {
-  googleScrape
+  googleScrape,
+  createGoogleJobObjects
 };
