933b4ff5eb370eb52d1e8f9de220b0704f7136ba
Josh Mabry <mabry1985@gmail.com>
2019-12-09 09:56:03 -0800

    add switch function for filtering job by title, add timestamp to save

--- utility/jobBoard.js
+++ utility/jobBoard.js
@@ -1,22 +1,35 @@
 const JobBoard = require("../models/JobBoard");
 
-async function save(title, description, postedBy, applyUrl, jobBoardSite) {
+async function save(title, description, postedBy, applyUrl,timeStamp, jobBoardSite) {
   const jobFromDb = await JobBoard.findOne({ description });
-  if (!jobFromDb && !title.includes("Senior") && !title.includes("Sr") && !title.includes("SR") ) {
+  if (!jobFromDb) {
     const jobBoard = new JobBoard({
       title,
       description,
       postedBy,
       applyUrl,
-      jobBoardSite
+      jobBoardSite,
+      timeStamp
     });
     console.log("job saved");
     return jobBoard.save();
-  } else {
-    console.log("duplicate job found");
+  }
+}
+
+function jobFilterTitle(job){
+  switch(job){
+    case job.includes("Senior"):
+      return true
+    case job.includes("Sr"):
+      return true
+    case job.includes("SR"):
+      return true
+    default:
+      return false;
   }
 }
 
 module.exports = {
-  save
+  save,
+  jobFilterTitle
 };
