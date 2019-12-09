5b4010c7df79a057fba837136d3dbf4c21a1a134
Josh Mabry <mabry1985@gmail.com>
2019-12-09 09:33:52 -0800

    get current tests passing

---
+++ __tests__/googleScraper.test.js
@@ -0,0 +1,30 @@
+const scraper = require("../scrapers/googleJobScraper");
+const fs = require("fs");
+let html;
+let jobs
+
+beforeAll(async (done) => {
+  html = fs.readFileSync("./test-html/googleJobBoard.html");
+  jobs = await scraper.createGoogleJobObjects(html);
+  done();
+});
+
+it("should give correct jobs object length", () => {
+    expect(jobs.length).toBe(21);
+})
+
+it("should get correct job title", async () => {
+  await expect(jobs[0].title)
+    .toBe("Full-Stack Software Engineer - Javascript");
+})
+
+it("should get correct url", () => {
+  expect(jobs[0].applyUrl)
+    .toBe("https://www.trinityventures.com/portfolio/new-relic"
+)
+})
+
+it("should get correct company", () => {
+  expect(jobs[0].postedBy).toBe("New Relic, Inc.")
+})
+
