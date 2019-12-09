b28cbd8d864c0bd6d350f73282ab326de55bbda0
Josh Mabry <mabry1985@gmail.com>
2019-12-09 12:48:14 -0800

    refactor createDiceJobObjects to use request, add timestamp property to object, return results from scrape

--- scrapers/diceScraper.js
+++ scrapers/diceScraper.js
@@ -1,11 +1,9 @@
 const cheerio = require("cheerio");
 const jobBoard = require("../utility/jobBoard");
-const downloadTestHtmlFile = false;
+const request = require("request-promise");
 
-async function createDiceJobObjects(jobPage, browser) {
-  const page = await browser.newPage();
-  await page.goto(jobPage, { waitUntil: "networkidle2", timeout: 0 });
-  const html = await page.evaluate(() => document.body.innerHTML);
+async function createDiceJobObjects(jobPage) {
+  const html = await request.get(jobPage);
   const $ = cheerio.load(html);
   const title = $("#header-wrap > div.container > div .jobTitle").text();
   const description = $("#jobdescSec")
@@ -13,7 +11,11 @@ async function createDiceJobObjects(jobPage, browser) {
     .trim();
   const postedBy = $("#hiringOrganizationName").text();
   const applyUrl = $("#applybtn").attr("onclick");
-  await jobBoard.save(title, description, postedBy, applyUrl, "Dice");
+  const jobBoardSite = "Dice";
+  const timeStamp = new Date();
+  await jobBoard.save(title, description, postedBy, applyUrl,  timeStamp, jobBoardSite);
+  return { title, description, postedBy, applyUrl, timeStamp, jobBoardSite
+  }
 }
 
 async function scrapeJobLinks(html) {
@@ -25,25 +27,32 @@ async function scrapeJobLinks(html) {
 }
 
 async function diceScrape(browser, queries) {
-  queries.map(async el => {
+ const resultsArray = queries.map(async el => {
     let search = el.query.replace(" ", "%20");
     let url = `https://www.dice.com/jobs?q=${search}&location=Portland,%20OR,%20USA&latitude=45.5051064&longitude=-122.67502609999997&countryCode=US&locationPrecision=City&radius=30&radiusUnit=mi&page=1&pageSize=20&facets=employmentType%7CpostedDate%7CworkFromHomeAvailability%7CemployerType%7CeasyApply&filters.postedDate=ONE&language=en`;
     try {
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: "networkidle2", timeout: 0 });
       const html = await page.evaluate(() => document.body.innerHTML);
-      // global switch variable
-      if (downloadTestHtmlFile) {
-        fs.writeFileSync("./test-html/diceJobBoard.html", html);
-      }
       const jobLinks = await scrapeJobLinks(html)
-      jobLinks.map(async jobPage => {
-        await createDiceJobObjects(jobPage, browser);
-      })
+      const jobsArray = jobLinks.map(jobPage => {
+        return new Promise(async(resolve, reject) =>{
+          const job = await createDiceJobObjects(jobPage, browser)
+          resolve(job);
+        });
+       })
+      const jobs = await Promise.all(jobsArray)
+        // .then(jobs => {console.log(jobs)});
+      // console.log(jobs);
+      return jobs
     } catch (err) {
       console.error(err);
     }
   });
+  let results = await Promise.all(resultsArray)
+  results = [].concat.apply([], results);
+  console.log(results)
+  return results
 }
 
 async function sleep(mseconds) {
